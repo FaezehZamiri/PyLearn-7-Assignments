7e0bdf2e02654636bd44fac9845930c9437baa0a
Faezeh Zamiri <74874616+FaezehZamiri@users.noreply.github.com>
2022-12-21 11:13:52 +0300

    Add files via upload

---
+++ 10/Assignment10.py
@@ -0,0 +1,49 @@
+class fraction:
+    def __init__(self,numerator,denominator):
+        self.numerator = numerator
+        self.denominator =denominator
+
+    def sum(self):
+        ...
+    def subtraction(self):
+        ...
+    def multiplication(self):
+        ...
+    def division(self):
+        ...
+    def show(self):
+        ...
+
+
+class time:
+    def __init__(self,hour,minute,seconds):
+        self.hour = hour
+        self.minute = minute
+        self.seconds = seconds
+
+    def sum(self):
+        ...
+    def subtraction(self):
+        ...
+    def time2seconds(self):
+        ...
+    def time2minute(self):
+        ...
+    def time2hour(self):
+        ...
+    def show(self):
+        ...
+
+
+class dateAD:
+    def __init__(self,year,month,day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def date2hijri(self):
+        ...
+    def subtraction(self):
+        ...
+    def show(self):
+        ...
