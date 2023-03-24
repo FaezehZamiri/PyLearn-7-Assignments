9efa16efd3a68dff78bad6d9ae3c3135ed77f538
Faezeh Zamiri <74874616+FaezehZamiri@users.noreply.github.com>
2023-03-24 16:15:12 +0300

    Update 2.py

--- 26/2.py
+++ 26/2.py
@@ -3,23 +3,19 @@ import cv2
 img_1 = cv2.imread('Input/1.jpg')
 img_2 = cv2.imread('Input/2.jpg')
 
-img_1 = cv2.cvtColor(img_1,cv2.COLOR_RGB2GRAY)
-img_2 = cv2.cvtColor(img_2,cv2.COLOR_RGB2GRAY)
-(row_1 , col_1)= img_1.shape
-(row_2 , col_2) = img_2.shape
+def Invert(img):
+    img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    (row , col)= img.shape
 
-for i in range(row_1):
-    for j in range(col_1):
-        img_1[i,j] = 255 - img_1[i,j]
+    for i in range(row):
+        for j in range(col):
+            img[i,j] = 255 - img[i,j]
 
-for i in range(row_2):
-    for j in range(col_2):
-        img_2[i,j] = 255 - img_2[i,j]
+    cv2.imshow('',img)
+    cv2.waitKey()
+    return img
 
-cv2.imshow('',img_1)
-cv2.waitKey()
-cv2.imshow('',img_2)
-cv2.waitKey()
-
+img_1=Invert(img_1)
+img_2=Invert(img_2)
 cv2.imwrite('Output/Invert_1.jpg',img_1)
 cv2.imwrite('Output/Invert_2.jpg',img_2)
